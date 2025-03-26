3e5e377a5be0e1e5b044310a1c2dfbbcc962bd29
Gabriel Cruz <159947061+cruzgb@users.noreply.github.com>
2025-03-26 00:08:08 -0300

    prints basicos
    hello world;
    fala curta.

---
+++ 01_output_comments/main.lua
@@ -0,0 +1,9 @@
+print("Hello, World!")
+--[[
+    this program
+    is a basic program,
+    to display some text on the screen
+]]
+-- prints text to the screen
+print("Hello, Juan! My name is Skyz.")
+print("Oi!")
